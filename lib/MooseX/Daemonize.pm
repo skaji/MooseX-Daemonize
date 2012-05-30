2358b232111f00c6986d8fe39c2c4a1723ba3201
Michael Reddick <michael.reddick@gmail.com>
2012-05-30 18:09:15 -0500

    bump the version to 0.14

--- lib/MooseX/Daemonize.pm
+++ lib/MooseX/Daemonize.pm
@@ -4,7 +4,7 @@ use Moose::Role;
 use MooseX::Types::Path::Class;
 use File::Path qw(make_path);
 
-our $VERSION   = '0.13';
+our $VERSION   = '0.14';
 
 with 'MooseX::Daemonize::WithPidFile',
      'MooseX::Getopt';
